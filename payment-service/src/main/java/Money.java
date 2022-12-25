8474ba75f2ebc002ccea623c137ebe8316b2c5cd
Vladimir <fominsky_v@live.com>
2022-12-25 18:52:09 +0500

    patterns_homework_6

--- payment-service/src/main/java/Money.java
+++ payment-service/src/main/java/Money.java
@@ -1,2 +1,17 @@
+import java.math.BigDecimal;
+import java.util.Currency;
+
 public class Money {
+    private final Currency currency;
+    private final BigDecimal amount;
+
+    public Money(Currency currency, BigDecimal amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+
+    public Money(Currency currency, double amount) {
+        this.currency = currency;
+        this.amount = BigDecimal.valueOf(amount).stripTrailingZeros();
+    }
 }
